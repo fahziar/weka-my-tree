5fe6fe5594335a650e6824f0db32fbdebf29998d
fahziar <fahziar_rw@yahoo.co.uk>
2015-09-29 23:26:27 +0700

    Initial commit

---
+++ src/main/java/tugas/Main.java
@@ -0,0 +1,169 @@
+package tugas;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.trees.Id3;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.MyId3;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
+import weka.filters.supervised.instance.Resample;
+
+import java.util.Random;
+
+/**
+ * Created by fahziar on 29/09/2015.
+ */
+public class Main {
+    private static final String DATASET_WEATHER = "weather.nominal.arff";
+    private static final String DATASET_WEATHER_NUM = "weather.numeric.arff";
+    private static final String DATASET_IRIS = "iris.arff";
+
+    private static Instances loadDataset(String location) throws Exception {
+        ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(location);
+        Instances data = dataSource.getDataSet();
+
+        if (data.classIndex() == -1){
+            data.setClassIndex(data.numAttributes() - 1);
+        }
+
+        return data;
+    }
+
+    private static Instances resample(Instances dataset) throws Exception {
+        Resample resample = new Resample();
+
+        String[] options = new String[3];
+        options[0] = "-B 0.0";
+        options[1] = "-S 1";
+        options[2] = "-Z 100.0";
+
+        resample.setOptions(options);
+        resample.setInputFormat(dataset);
+        return Filter.useFilter(dataset, resample);
+
+    }
+
+    public static void main(String[] args){
+        Instances irisDs;
+        Instances weatherDs;
+        Instances numWeatherDs;
+
+        //Load dataset
+        try {
+            System.out.println("Loading dataset dataset");
+            weatherDs = loadDataset(DATASET_WEATHER);
+            numWeatherDs = loadDataset(DATASET_WEATHER_NUM);
+            irisDs = loadDataset(DATASET_IRIS);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Failed to load dataset");
+            return;
+        }
+
+        //Resample dataset
+        try {
+            System.out.println("Filtering dataset");
+//            weatherDs = resample(weatherDs);
+//            numWeatherDs = resample(numWeatherDs);
+//            irisDs = resample(irisDs);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Failted to filter dataset");
+            return;
+        }
+
+        //Build classifier
+        System.out.println("Building classifier");
+
+        MyId3 myId3Weather = new MyId3();
+        MyId3 myId3Iris = new MyId3();
+
+        Id3 id3Weather = new Id3();
+        Id3 id3Iris = new Id3();
+
+        J48 j48Weather = new J48();
+        J48 j48NumWeather = new J48();
+        J48 j48Iris = new J48();
+
+        NaiveBayes naiveWeather = new NaiveBayes();
+        NaiveBayes naiveNumWeather = new NaiveBayes();
+        NaiveBayes naiveIris = new NaiveBayes();
+
+
+        try {
+
+            myId3Weather.buildClassifier(weatherDs);
+//            myId3Iris.buildClassifier(irisDs);
+
+            id3Weather.buildClassifier(weatherDs);
+            //id3Iris.buildClassifier(irisDs);
+
+            j48Weather.buildClassifier(weatherDs);
+            j48NumWeather.buildClassifier(numWeatherDs);
+            j48Iris.buildClassifier(irisDs);
+
+            naiveWeather.buildClassifier(weatherDs);
+            naiveNumWeather.buildClassifier(numWeatherDs);
+            naiveIris.buildClassifier(irisDs);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to build classifier");
+            return;
+        }
+
+        //10 cross validation dataset
+        try {
+            Evaluation weatherEval = new Evaluation(weatherDs);
+            Evaluation weatherNumEval = new Evaluation(numWeatherDs);
+            Evaluation irisEval = new Evaluation(irisDs);
+
+            //My ID3
+//            weatherEval.crossValidateModel(myId3Weather, weatherDs, 10, new Random(1));
+            System.out.println(weatherEval.toSummaryString("\nMyID3 Weathenr Nominal Results\n======\n", false));
+//            irisEval.crossValidateModel(myId3Iris, irisDs, 10, new Random(1));
+//            System.out.println(irisEval.toSummaryString("\nMyID3 iris Results\n======\n", false));
+
+            //ID3
+            weatherEval.crossValidateModel(id3Weather, weatherDs, 10, new Random(1));
+            System.out.println(weatherEval.toSummaryString("\nID3 Weathenr Nominal Results\n======\n", false));
+//            irisEval.crossValidateModel(id3Iris, irisDs, 10, new Random(1));
+//            System.out.println(irisEval.toSummaryString("\nID3 iris Results\n======\n", false));
+
+            //J48
+            weatherEval.crossValidateModel(j48Weather, weatherDs, 10, new Random(1));
+            System.out.println(weatherEval.toSummaryString("\nJ48 Weather Nominal Results\n======\n", false));
+            weatherNumEval.crossValidateModel(j48NumWeather, numWeatherDs, 10, new Random(1));
+            System.out.println(weatherEval.toSummaryString("\nJ48 Weather Numeric Results\n======\n", false));
+            irisEval.crossValidateModel(j48Iris, irisDs, 10, new Random(1));
+            System.out.println(irisEval.toSummaryString("\nJ48 iris Results\n======\n", false));
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Failed to evaluate dataset");
+            return;
+        }
+
+        //Save model
+        try {
+            System.out.println("Saving model");
+            SerializationHelper.write("myId3Weather.model", myId3Weather);
+//            SerializationHelper.write("myId3Iris.model", myId3Iris);
+
+            SerializationHelper.write("Id3Weather.model", id3Weather);
+//            SerializationHelper.write("Id3Iris.model", id3Iris);
+
+            SerializationHelper.write("J48Weather.model", j48Weather);
+            SerializationHelper.write("J48NumWeather.model", j48NumWeather);
+            SerializationHelper.write("J48Iris.model", j48Iris);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to save model");
+        }
+    }
+}
---
+++ src/main/java/weka/classifiers/trees/MyId3.java
@@ -0,0 +1,256 @@
+package weka.classifiers.trees;
+
+import weka.classifiers.Classifier;
+import weka.core.*;
+
+import java.io.Serializable;
+import java.util.Enumeration;
+
+/**
+ * Created by fahziar on 28/09/2015.
+ */
+public class MyId3 extends Classifier implements Serializable {
+    private final double CLASS_VALUE_NOT_LEAF = -99.0;
+    private Attribute m_Attribute; //Atrribute of current node
+    private MyId3[] m_Branches; //Branches from current node; null if leaf
+//    private double[] m_Distribution;
+    private double m_ClassValue;
+    private Attribute m_ClassAtribute;
+
+    public MyId3(){
+        super();
+        m_ClassValue = CLASS_VALUE_NOT_LEAF;
+    }
+
+
+    @Override
+    public void buildClassifier(Instances instance) throws Exception {
+        Instances data = new Instances(instance);
+        getCapabilities().testWithFail(data);
+
+        data.deleteWithMissingClass();
+        makeTree(data);
+    }
+
+    @Override
+    public Capabilities getCapabilities() {
+        Capabilities out =  super.getCapabilities();
+        out.disableAll();
+
+        out.enable(Capabilities.Capability.NOMINAL_ATTRIBUTES);
+
+        out.enable(Capabilities.Capability.NOMINAL_CLASS);
+        out.enable(Capabilities.Capability.MISSING_CLASS_VALUES);
+
+        return out;
+    }
+
+    private double calculateEntropy(Instances data){
+//        double classCounts[] = new double[data.numClasses()];
+//        Enumeration instEnum = data.enumerateInstances();
+//        while (instEnum.hasMoreElements()){
+//            Instance i = (Instance) instEnum.nextElement();
+//            classCounts[(int) i.classValue()]++;
+//        }
+//
+//        double entropy = 0.0;
+//        for (int i=0; i<classCounts.length; i++){
+//            if (classCounts[i] > 0) {
+//                double probability = classCounts[i] / data.numInstances();
+//                entropy -=  probability * Utils.log2(probability);
+//            }
+//        }
+//        return entropy;
+
+        double [] classCounts = new double[data.numClasses()];
+        Enumeration instEnum = data.enumerateInstances();
+        while (instEnum.hasMoreElements()) {
+            Instance inst = (Instance) instEnum.nextElement();
+            classCounts[(int) inst.classValue()]++;
+        }
+        double entropy = 0;
+        for (int j = 0; j < data.numClasses(); j++) {
+            if (classCounts[j] > 0) {
+                entropy -= classCounts[j] * Utils.log2(classCounts[j]);
+            }
+        }
+        entropy /= (double) data.numInstances();
+        return entropy + Utils.log2(data.numInstances());
+    }
+
+    private double computeEntropy(Instances data){
+        double classCounts[] = new double[data.numClasses()];
+        Enumeration instEnum = data.enumerateInstances();
+        while (instEnum.hasMoreElements()){
+            Instance i = (Instance) instEnum.nextElement();
+            classCounts[(int) i.classValue()]++;
+        }
+
+        double entropy = 0.0;
+        for (int i=0; i<classCounts.length; i++){
+            if (classCounts[i] > 0) {
+                double probability = classCounts[i] / data.numInstances();
+                entropy -=  probability * Utils.log2(probability);
+            }
+        }
+        return entropy;
+
+    }
+
+    public Instances[] splitInstances(Instances instances, Attribute attribute){
+        Instances[] out = new Instances[attribute.numValues()];
+
+        for (int i=0; i<attribute.numValues(); i++){
+            out[i] = new Instances(instances, instances.numInstances());
+        }
+
+        Enumeration instEnum = instances.enumerateInstances();
+        while (instEnum.hasMoreElements()){
+            Instance i = (Instance) instEnum.nextElement();
+            out[(int) i.value(attribute)].add(i);
+        }
+
+        for (int i=0; i<out.length; i++){
+            out[i].compactify();
+        }
+
+        return out;
+
+    }
+
+    private Instances[] splitData(Instances data, Attribute att) {
+
+        Instances[] splitData = new Instances[att.numValues()];
+        for (int j = 0; j < att.numValues(); j++) {
+            splitData[j] = new Instances(data, data.numInstances());
+        }
+        Enumeration instEnum = data.enumerateInstances();
+        while (instEnum.hasMoreElements()) {
+            Instance inst = (Instance) instEnum.nextElement();
+            splitData[(int) inst.value(att)].add(inst);
+        }
+        for (int i = 0; i < splitData.length; i++) {
+            splitData[i].compactify();
+        }
+        return splitData;
+    }
+
+//    private double getInfoGain(double entropy, int numData, Instances[] splitData, Attribute att){
+//        double infoGain = entropy;
+//
+//        for (int i=0; i<att.numValues(); i++){
+//            infoGain -= ((double) splitData[i].numInstances() / numData) * calculateEntropy(splitData[i]);
+//        }
+//
+//        return infoGain;
+//    }
+
+    private double computeInfoGain(Instances data, Attribute att)
+            throws Exception {
+
+        double infoGain = computeEntropy(data);
+        Instances[] splitData = splitData(data, att);
+        for (int j = 0; j < att.numValues(); j++) {
+            if (splitData[j].numInstances() > 0) {
+                infoGain -= ((double) splitData[j].numInstances() /
+                        (double) data.numInstances()) *
+                        computeEntropy(splitData[j]);
+            }
+        }
+        return infoGain;
+    }
+
+    @Override
+    public double classifyInstance(Instance instance) throws Exception {
+        if (instance.hasMissingValue()) {
+            throw new NoSupportForMissingValuesException("Id3: no missing values, "
+                    + "please.");
+        }
+        if (isLef()) {
+            return m_ClassValue;
+        } else {
+            return m_Branches[(int) instance.value(m_Attribute)].classifyInstance(instance);
+        }
+    }
+
+    private void makeTree(Instances data){
+        double entropy = calculateEntropy(data);
+        double infoGain[] = new double[data.numAttributes()];
+
+        //Calculate information gain
+        Enumeration attEnum = data.enumerateAttributes();
+        while (attEnum.hasMoreElements()){
+            Attribute att = (Attribute) attEnum.nextElement();
+            Instances[] splitData = splitData(data, att);
+//            infoGain[att.index()] = getInfoGain(entropy, data.numInstances(), splitData, att);
+            try {
+                infoGain[att.index()] = computeInfoGain(data, att);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Select max attribute
+        m_Attribute = data.attribute(Utils.maxIndex(infoGain));
+
+        //Make branch or leaf
+        if (Utils.eq(infoGain[m_Attribute.index()], 0)){
+            m_Attribute = null;
+
+            //create a leaf
+//            m_Distribution = new double[data.numClasses()];
+//
+//            Enumeration instEnum = data.enumerateInstances();
+//            while (instEnum.hasMoreElements()){
+//                Instance i = (Instance) instEnum.nextElement();
+//                m_Distribution[(int) i.classValue()]++;
+//            }
+//
+//            Utils.normalize(m_Distribution);
+            m_ClassValue = data.instance(0).classValue();
+            m_ClassAtribute = data.classAttribute();
+
+        } else {
+            Instances[] split = splitInstances(data, m_Attribute);
+            m_Branches = new MyId3[m_Attribute.numValues()];
+            for (int i=0; i<m_Attribute.numValues(); i++){
+                m_Branches[i] = new MyId3();
+                m_Branches[i].makeTree(split[i]);
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        if ((Utils.eq(m_ClassValue, CLASS_VALUE_NOT_LEAF)) && (m_Branches == null)) {
+            return "Id3: No model built yet.";
+        }
+        return "Id3\n\n" + toString(0);
+    }
+
+    private String toString(int level) {
+
+        StringBuffer text = new StringBuffer();
+
+        if (m_Attribute == null) {
+            text.append(": " + m_ClassAtribute.value((int) m_ClassValue));
+
+        } else {
+            for (int j = 0; j < m_Attribute.numValues(); j++) {
+                text.append("\n");
+                for (int i = 0; i < level; i++) {
+                    text.append("|  ");
+                }
+                text.append(m_Attribute.name() + " = " + m_Attribute.value(j));
+                text.append(m_Branches[j].toString(level + 1));
+            }
+        }
+        return text.toString();
+    }
+
+    private boolean isLef(){
+        return m_Branches == null;
+    }
+
+}
